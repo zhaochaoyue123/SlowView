5e33be143f81757f64a1ba3854a82fa49905504e
zhaochaoyue <770812088@qq.com>
2020-09-09 21:55:30 +0800

    Initial commit

---
+++ slowviewlib/src/main/java/com/zcy/slowviewlib/MySlowView.java
@@ -0,0 +1,21 @@
+package com.zcy.slowviewlib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MySlowView extends View {
+    public MySlowView(Context context) {
+        super(context);
+    }
+
+    public MySlowView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MySlowView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
